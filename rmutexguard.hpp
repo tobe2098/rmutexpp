5c2a8c6adf60400b81686a855814d0ecc7a8e0ac
Antoni Bertolin <tonibm1220@gmail.com>
2025-05-25 03:20:08 +0200

    Final version

--- rmutexguard.hpp
+++ rmutexguard.hpp
@@ -54,7 +54,7 @@ class RMutexGuard {
     }
 
     // Constructor for try locking of RMutex objects
-    RMutexGuard(std::try_to_lock_t, Ts&... mutexes):
+    [[nodiscard]] RMutexGuard(std::try_to_lock_t, Ts&... mutexes):
         _owns_locks(false), _locks(std::make_tuple(std::unique_lock<rmutex_mutex_type_t<Ts>>(mutexes._internal_mutex, std::defer_lock)...)),
         _mutex_refs(mutexes...) {
       try_lock_all(std::index_sequence_for<Ts...> {});
@@ -87,9 +87,9 @@ class RMutexGuard {
     RMutexGuard& operator=(const RMutexGuard&) = delete;
 
     // Check if owns all locks
-    bool     owns_locks() const& noexcept { return _owns_locks; }
+    bool     owns() const& noexcept { return _owns_locks; }
     explicit operator bool() const& noexcept { return _owns_locks; }
-    bool     owns_locks() const&&    = delete;
+    bool     owns() const&&          = delete;
     explicit operator bool() const&& = delete;
 
     bool try_lock() const& { return try_lock_all(std::index_sequence_for<Ts...> {}); }
@@ -116,8 +116,74 @@ class RMutexGuard {
 template <typename T>
   requires all_are_rmutex<T>
 class RMutexGuard<T> {
-    T&                     rmutex;
-    rmutex_data_type_t<T>& data_ref;
+    mutable std::unique_lock<std::mutex> _lock;
+    rmutex_data_type_t<T>&               _data_ref;
+    mutable bool                         _owns_lock;
+
+  public:
+    // Constructor for regular locking of RMutex objects
+    [[nodiscard]] explicit RMutexGuard(T& mutex): _owns_lock(true), _lock(mutex._internal_mutex), _data_ref(mutex._internal_data) { }
+
+    // Constructor for try locking of RMutex objects
+    [[nodiscard]] RMutexGuard(std::try_to_lock_t, T& mutex):
+        _owns_lock(false), _lock(mutex._internal_mutex, std::try_to_lock), _data_ref(mutex._internal_data) {
+      _owns_lock = _lock.owns_lock();
+    }
+
+    // Default constructor
+    // [[nodiscard]] RMutexGuard(): _owns_locks(false) { }
+
+    // Destructor
+    ~RMutexGuard() = default;
+
+    // Move operations
+    RMutexGuard(RMutexGuard&& other) noexcept: _lock(std::move(other._lock)), _data_ref(other._data_ref), _owns_lock(other._owns_lock) {
+      other._owns_lock = false;
+    }
+
+    RMutexGuard& operator=(RMutexGuard&& other) noexcept {
+      if (this != &other) {
+        _lock            = std::move(other._lock);
+        _data_ref        = _data_ref;
+        _owns_lock       = other._owns_lock;
+        other._owns_lock = false;
+      }
+      return *this;
+    }
+
+    // Copy operations are deleted
+    RMutexGuard(const RMutexGuard&)            = delete;
+    RMutexGuard& operator=(const RMutexGuard&) = delete;
+
+    // Check if owns all locks
+    bool     owns() const& noexcept { return _owns_lock; }
+    explicit operator bool() const& noexcept { return _owns_lock; }
+    bool     owns() const&&          = delete;
+    explicit operator bool() const&& = delete;
+
+    bool try_lock() const& { return _owns_lock = _lock.try_lock(); }
+    void lock() const& {
+      _lock.lock();
+      _owns_lock = true;
+    }
+
+    std::optional<rmutex_data_type_t<T>&> get_data() & {
+      if (_owns_lock) {
+        return _data_ref;
+      } else {
+        return std::nullopt;
+      }
+    }
+    std::optional<const rmutex_data_type_t<T>&> get_data() const& {
+      if (_owns_lock) {
+        return std::cref(_data_ref);
+      } else {
+        return std::nullopt;
+      }
+    }
+    // Explicitly delete rvalue versions to prevent temporary access
+    std::optional<rmutex_data_type_t<T>&>       get_data() &&      = delete;
+    std::optional<const rmutex_data_type_t<T>&> get_data() const&& = delete;
 };
 
 // Deduction guide - allows the compiler to deduce template arguments from constructor arguments
